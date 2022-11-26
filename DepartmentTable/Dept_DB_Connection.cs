cf1d8592272859fa485a9dd53b27ff5f6ab9a872
Durga Anaparthi <Durga Anaparthi@LAPTOP-4UV87UTN>
2022-11-26 15:19:07 +0530

    Add project files.

---
+++ DepartmentTable/Dept_DB_Connection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepartmentTable
+{
+    internal class Dept_DB_Connection 
+    {
+        string connetionString = "";
+        SqlCommand cmd;
+        SqlConnection SqlCon;
+        string query = "";
+        public void Method1()
+        {
+            connetionString = "Server=LAPTOP-4UV87UTN;Database=Durgabhavani;Trusted_Connection=true;";
+            query = "Insert into Department  Values()";
+            SqlCon.Open();
+            SqlCon.Close();
+        }
+    }
+}
---
+++ DepartmentTable/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DepartmentTable;
+using System.Data.SqlClient;
+using System.Reflection;
+
+Dept_List departmentListobj = new Dept_List();
+var departmentList = departmentListobj.Method();
+SqlConnection connectionstring = new SqlConnection("Server=LAPTOP-4UV87UTN;Database=Durgabhavani;Trusted_Connection=true;");
+string query = "Insert Into Department (DeptName, DeptShortName) " +
+                   "VALUES (@FN, @LN) ";
+foreach (var dept in departmentList)
+{
+    using (SqlCommand cmd = new SqlCommand(query, connectionstring))
+    {
+        // add parameters and their values
+        cmd.Parameters.Add("@FN", System.Data.SqlDbType.NVarChar, 100).Value = dept.DeptName;
+        cmd.Parameters.Add("@LN", System.Data.SqlDbType.NVarChar, 100).Value = dept.DeptShortName;
+
+        connectionstring.Open();
+        cmd.ExecuteNonQuery();
+        connectionstring.Close();
+    }
+}
